13074cbc3b9cf2813105b6bd6fceeb431ec25cb4
jose ignacio decima <jidecima87@gmail.com>
2013-03-24 11:56:23 -0300

    seccion contactenos

---
+++ contactenos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE>
+<html lang="es">
+<head>
+	<meta charset="utf-8" />
+	<title>Pelucas Graciela Ani 100% Pelo Natural</title>
+
+	<link rel="stylesheet" href="normalize.css">
+	<link rel="stylesheet" href="estilos.css">
+
+	<script src="prefixfree.min.js"></script>
+	<script src="jquery.js"></script>
+</head>
+<body>
+	
+	<?php include ('infobar.php'); ?>
+	
+	<?php include ('header.php'); ?>
+
+	<?php include ('nav.php'); ?>
+	
+	<section>
+		<article id="quienessomos">
+			<p>Peluqueria tradicional familiar, anteriormente ubicada en esquina San juan y Catamarca. La segunda peluquería creada en Tucumán para mujeres desde el año 1940.</p>
+			<p>Este privilegio viene de generación en generación mejorando su capacitación profesional día a día y garantizandole a usted el mejor servicio de toda la provincia hasta el día de hoy.</p>
+		</article>
+		<div id="mapa">
+			<a>prueba</a>
+
+		</div>
+	</section>
+	<footer>Graciela Ani | San Juan 1708 | Celular: 3814448001 Fijo: 0381 – 4332736 | San Miguel de Tucumán. Argentina.
+	</footer>
+</body>
+</html>
